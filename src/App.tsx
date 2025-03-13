70c3281f5a2f028cbdf20227988400acd943f6e0
kiowesmith21 <kiowesmith@gmail.com>
2025-03-13 18:38:00 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+// import Login from './pages/Login';
+// import Search from './pages/Search';
+// import Favorites from './pages/Favorites';
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Login />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/favorites" element={<Favorites />} /> */}
+    </Routes>
+  );
+}
+
+export default App;
